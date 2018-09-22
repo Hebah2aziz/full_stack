07bda5c4aae1d99f67fb9bdf53e6d16c21506ef3
hebah2aziz <hebah2aziz@gmail.com>
2018-09-22 14:48:48 +0300

    thrid

--- movies/intertainment_center.py
+++ movies/intertainment_center.py
@@ -1,16 +1,15 @@
-
 import fresh_tomatoes
 import media
-toystory=media.Movies("toystory","the boy and his toys come a live","https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=rNk1Wi8SvNc")
-#print(toystory.storyline)
-#toystory.show_trailer()
 
-the_lord=media.Movies("the lord of the rings","the story of bravy guy try to save the middel earth",
-                      "https://upload.wikimedia.org/wikipedia/en/e/e9/First_Single_Volume_Edition_of_The_Lord_of_the_Rings.gif","https://youtu.be/rCZ3SN65kIs")
-#print(the_lord.storyline)
-#the_lord.show_trailer()
+toystory = media.Movies("toystory",
+                        "the boy and his toys come a live",
+                        "https://goo.gl/j7nkk9",
+                        "https://www.youtube.com/watch?v=rNk1Wi8SvNc")
+the_lord = media.Movies("the lord of the rings",
+                        "the story of bravy guy try to save the middel earth",
+                        "https://goo.gl/VSGgVM",
+                        "https://youtu.be/rCZ3SN65kIs")
 
-movies={toystory,the_lord}#create varible and send two movies objects
-fresh_tomatoes.open_movies_page(movies)#use fresh_tomato to style web page 
 
- 
+movies = {toystory, the_lord}  # create varible and send two movies objects
+fresh_tomatoes . open_movies_page(movies)  # use fresh_tomato to style webpage
